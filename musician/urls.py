dcc796ff4cce645f91fb39c44e77dd2292ac2ee6
Yura Push <youdeimos1@gmail.com>
2025-01-29 14:39:48 +0200

    add router for musician api

--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,14 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework import routers
+
+from musician.views import MusicianViewSet
+
+
+router = routers.DefaultRouter()
+router.register("musician", MusicianViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
 
 app_name = "musician"
