df14bfc88d91d3c7687cdb069010bb7160ae357d
Yura Push <youdeimos1@gmail.com>
2025-01-29 14:40:28 +0200

    minor changes

--- musician/tests/test_musician_api.py
+++ musician/tests/test_musician_api.py
@@ -7,7 +7,7 @@ from rest_framework.test import APIClient
 from musician.models import Musician
 from musician.serializers import MusicianSerializer
 
-MUSICIAN_URL = reverse("musician:manage-list")
+MUSICIAN_URL = reverse("musician:musician-list")
 
 
 class MusicianApiTests(TestCase):
