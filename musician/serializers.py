e3828decc4da76702016702693ce7d2996a7702e
Yura Push <youdeimos1@gmail.com>
2025-01-29 14:38:41 +0200

    instantiate musician serializer

---
+++ musician/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from musician.models import Musician
+
+
+class MusicianSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Musician
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "instrument",
+            "age",
+            "date_of_applying",
+            "is_adult",
+        ]
