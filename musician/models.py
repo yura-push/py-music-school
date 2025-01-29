9d79960daee4803203db6eb0c42c7f205a27de35
Yura Push <youdeimos1@gmail.com>
2025-01-29 14:41:13 +0200

    flake8 changes

--- musician/models.py
+++ musician/models.py
@@ -6,7 +6,7 @@ class Musician(models.Model):
     first_name = models.CharField(max_length=63)
     last_name = models.CharField(max_length=63)
     instrument = models.CharField(max_length=63)
-    age = models.IntegerField(validators=[MinValueValidator(14),])
+    age = models.IntegerField(validators=[MinValueValidator(14), ])
     date_of_applying = models.DateField(auto_now_add=True)
 
     def __str__(self):
