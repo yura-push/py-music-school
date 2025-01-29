48cae648010e3a6528fa69b4532ba17dda178c09
Yura Push <youdeimos1@gmail.com>
2025-01-29 14:38:00 +0200

    create Musician model

--- musician/models.py
+++ musician/models.py
@@ -1,5 +1,17 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 class Musician(models.Model):
-    pass
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+    instrument = models.CharField(max_length=63)
+    age = models.IntegerField(validators=[MinValueValidator(14),])
+    date_of_applying = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    @property
+    def is_adult(self) -> bool:
+        return self.age >= 21
